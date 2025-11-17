c4b3e71b70868d4b85b80b6f8067b59f79cfad23
bon0512 <nonstop00450@gmail.com>
2025-11-17 17:14:08 +0900

    chapter last

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,23 +9,23 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-@Configuration
-@ComponentScan(
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
-         classes = Configuration.class)
-)
-public class AutoAppConfig {
-
-    @Bean
-    public DiscountPolicy fixDiscountPolicy() {
-        return new fixDiscountPolicy();
-    }
-
-    @Bean
-    public DiscountPolicy rateDiscountPolicy() {
-        return new RateDiscountPolicy();
-    }
-
-
-
-}
+//@Configuration
+//@ComponentScan(
+//        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
+//         classes = Configuration.class)
+//)
+//public class AutoAppConfig {
+//
+//    @Bean
+//    public DiscountPolicy fixDiscountPolicy() {
+//        return new fixDiscountPolicy();
+//    }
+//
+//    @Bean
+//    public DiscountPolicy rateDiscountPolicy() {
+//        return new RateDiscountPolicy();
+//    }
+//
+//
+//
+//}
---
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,47 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind(){
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+
+        prototypeBean1.addCount();
+
+
+
+
+    }
+
+
+    @Scope("prototype")
+    static class PrototypeBean{
+        private int count  = 0;
+
+        public void addCount(){
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init"+this);
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
